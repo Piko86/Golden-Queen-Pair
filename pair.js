2b5128588b347303df7e581cf7e2fcf3bd71a5bf
Piko86 <manmithabackup@gmail.com>
2025-09-30 22:16:45 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -12,7 +12,7 @@ function removeFile(FilePath) {
 }
 
 async function getMongoClient() {
-    const uri = process.env.MONGO_URI || 'mongodb://mongo:nPwqJATaorIUugKMxtLdwNogiVXymhmz@yamanote.proxy.rlwy.net:21462';
+    const uri = process.env.MONGO_URI || 'mongodb+srv://manmithabackup:manmitha@123@cluster0.gb9vdcn.mongodb.net/';
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     await client.connect();
     return client;
